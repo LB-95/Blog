d323d0546072024bf37774318c8b71ed84a903b1
Leo <leo.bravar95@gmail.com>
2021-05-21 00:43:44 +0200

    f

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "leo", password: "1488", 
+  except: [:index, :show]
+
   def index
     @articles = Article.all
   end
